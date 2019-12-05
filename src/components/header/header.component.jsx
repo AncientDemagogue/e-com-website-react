16f7f90f45790610bfb88503a07255c017c700d7
Domagoj Bakota <domagojbakota@gmail.com>
2019-12-06 00:18:27 +0100

    added cartIcon and cartDropdown components to our application, also implemented reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,8 +7,10 @@ import './header.styles.scss';
 
 import {ReactComponent as Logo} from '../../assets/4.4 crown.svg.svg';
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -29,12 +31,16 @@ const Header = ({currentUser}) => (
           SIGN IN{' '}
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser,
+// nested destructuring, using color and another sqigly brackets we are destructuring currentuser inside the user value
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
